ad8635efd4737120d688cd59c71e8fdbd68621c1
SFelbeck <stefanfelbeck@gmail.com>
2022-05-22 23:29:41 -0700

    initial commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, validate: [ isEmail, 'invalid email']},
+        thoughts: Number,
+        friends: Number
+    }
+)
