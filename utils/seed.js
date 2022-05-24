aa02334afb1e8abae64a1a9ddfe3c4c83965258b
SFelbeck <stefanfelbeck@gmail.com>
2022-05-24 11:10:03 -0700

    initial seeding code

--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,25 @@
 //empty for now
+const connection = require('../config/connection');
+const { User, Thought, Reaction } = require('../models');
+
+const { genRandomIndex, getRandomUserName } = require('./data');
+
+//import functions for seed data
+
+console.time('seeding');
+
+connection.once('open', async () => {
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+    await Reaction.deleteMany({});
+
+    const users = [];
+    const thoughts =[];
+    const reactions = [];
+
+    const makeThought = (text) => {
+        thougths.push({
+//idk what to put here
+        })
+    }
+})
